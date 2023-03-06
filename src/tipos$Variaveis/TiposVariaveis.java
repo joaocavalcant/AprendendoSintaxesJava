4594443b1df2c1d1d24f3421c151a3876258d94a
João <joaoaugustocavalcanti@hotmail.com>
2023-03-06 12:15:57 -0300

    second commit

---
+++ src/tipos$Variaveis/TiposVariaveis.java
@@ -0,0 +1,11 @@
+package tipos$Variaveis;
+
+public class TiposVariaveis {
+	public static void main(String[] args) {
+		double salarioMinimo = 2500;
+		
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short) numeroNormal;
+	}
+}
