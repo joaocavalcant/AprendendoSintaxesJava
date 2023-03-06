b1d51b576dfe8a56d455c4c0f285071a9b6d772a
João <joaoaugustocavalcanti@hotmail.com>
2023-03-06 11:31:48 -0300

    first commit

---
+++ src/AnatomiaClasses/MinhaClasse.java
@@ -0,0 +1,68 @@
+package AnatomiaClasses;
+
+public class MinhaClasse {
+		
+		// System.out.println("Olá turma, sejam bem vindos");
+
+	    // Estrutura de declaração de variáveis
+
+	    // Tipo NomeBemDefinido = Valor ou nome atribuido (opcional em alguns casos)
+
+	    // int idade = 23;
+	    // double altura = 1.62;
+
+	    // String meuNome = "João";
+
+	    // int anoDeFabricacao = 2022;
+
+	    // boolean verdadeira = true; //ou false;
+
+	    // anoDeFabricacao = 2018;
+
+	    /*-------Metodos------- */
+
+
+	public static void main(String[] args) {
+		
+		// Estrutura de declaração de variáveis
+
+	    // Tipo NomeBemDefinido = Valor ou nome atribuido (opcional em alguns casos)
+
+		
+		/*Tipo ->*/ String  
+		
+		/*Nome bem definido ->*/primeiroNome = 
+		
+		/*Valor ou nome atribuido ->*/ "João";
+		
+		String segundoNome = " Cavalcanti";
+		
+		String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+		
+		System.out.println(nomeCompleto);
+	}
+	
+	
+	// Estrutura de declaração de métodos
+	
+	// TipoRetorno NomeObjetivoNoInfinitivo (Paramentros)
+	
+	//Exemplo 
+	
+	/*Tipo de retorno ->*/public static String 
+	
+	/*NomeObjetivoDefinido ->*/nomeCompleto
+	
+	/*Parametros a serem utilizados ->*/ (String primeiroNome, String segundoNome) {
+		
+		return "Resultado do método " + primeiroNome + segundoNome;
+		
+	
+}
+	
+}	    
+
+	    
+
+
+		
