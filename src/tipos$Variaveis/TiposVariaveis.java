e29669d4bd497a8585851d0c538fdb12b7f04458
João <joaoaugustocavalcanti@hotmail.com>
2023-03-06 12:23:49 -0300

    second commit

--- src/tipos$Variaveis/TiposVariaveis.java
+++ src/tipos$Variaveis/TiposVariaveis.java
@@ -7,5 +7,25 @@ public class TiposVariaveis {
 		short numeroCurto = 1;
 		int numeroNormal = numeroCurto;
 		short numeroCurto2 = (short) numeroNormal;
+		
+		/*-----------Constantes---------------*/
+		
+		/* As constantes são valores armazenados em memória
+		 * que não podem ser modificados depois de declarados
+		 * em Java esses valores são representados pela palavra
+		 * reservada "final" todas as contantes são 
+		 * representadas totalmente em letra maiuscula
+		 */
+		
+		/* representacao de uma variavel*/
+		int numero = 5;
+		
+		numero = 10;
+		
+		System.out.println(numero);
+		
+		final double VALOR_DE_PI = 3.14;
+		
+		
 	}
 }
